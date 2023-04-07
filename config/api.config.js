6d39777d6e80a9b152380f93466dab48e431dd97
yousaihanser <130153561+yousaihanser@users.noreply.github.com>
2023-04-07 20:15:25 +0800

    Update api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -1,34 +1,64 @@
 /**
- * This file contains the configuration for the API endpoints and tokens we use.
- *
- * - If you are a OneDrive International user, you would not have to change anything here.
- * - If you are not the admin of your OneDrive for Business account, you may need to define your own clientId/clientSecret,
- *   check documentation for more details.
- * - If you are using a E5 Subscription OneDrive for Business account, the direct links of your files are not the same here.
- *   In which case you would need to change directLinkRegex.
+ * This file contains the configuration used for customising the website, such as the folder to share,
+ * the title, used Google fonts, site icons, contact info, etc.
  */
 module.exports = {
-  // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
-  // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-  clientId: 'd87bcc39-1750-4ca0-ad54-f8d0efbb2735',
-  obfuscatedClientSecret: 'U2FsdGVkX1830zo3/pFDqaBCVBb37iLw3WnBDWGF9GIB2f4apzv0roemp8Y+iIxI3Ih5ecyukqELQEGzZlYiWg==',
+  // This is what we use to identify who you are when you are initialising the website for the first time.
+  // Make sure this is exactly the same as the email address you use to sign into your Microsoft account.
+  // You can also put this in your Vercel's environment variable 'NEXT_PUBLIC_USER_PRINCIPLE_NAME' if you worry about
+  // your email being exposed in public.
+  userPrincipalName: process.env.NEXT_PUBLIC_USER_PRINCIPLE_NAME || 'kissdyc@u1s1.onmicrosoft.com',
 
-  // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
-  // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
-  redirectUri: 'http://localhost',
+  // [OPTIONAL] This is the website icon to the left of the title inside the navigation bar. It should be placed under the
+  // /public directory of your GitHub project (not your OneDrive folder!), and referenced here by its relative path to /public.
+  icon: '/icons/1.png',
 
-  // These are the URLs of the OneDrive API endpoints. You would not need to change anything here if you are using OneDrive International
-  // or E5 Subscription OneDrive for Business. You may need to change these if you are using OneDrive 世纪互联.
-  authApi: 'https://login.microsoftonline.com/common/oauth2/v2.0/token',
-  driveApi: 'https://graph.microsoft.com/v1.0/me/drive',
+  // Prefix for KV Storage
+  kvPrefix: process.env.KV_PREFIX || '',
 
-  // The scope we require are listed here, in most cases you would not need to change this as well.
-  scope: 'user.read files.read.all offline_access',
+  // The name of your website. Present alongside your icon.
+  title: "秋雨",
 
-  // Cache-Control header, check Vercel documentation for more details. The default settings imply:
-  // - max-age=0: no cache for your browser
-  // - s-maxage=0: cache is fresh for 60 seconds on the edge, after which it becomes stale
-  // - stale-while-revalidate: allow serving stale content while revalidating on the edge
-  // https://vercel.com/docs/concepts/edge-network/caching
-  cacheControlHeader: 'max-age=0, s-maxage=60, stale-while-revalidate',
+  // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
+  baseDirectory: '/Public',
+
+  // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
+  // Do note that this is limited up to 200 items by the upstream OneDrive API.
+  maxItems: 100,
+
+  // [OPTIONAL] We use Google Fonts natively for font customisations.
+  // You can check and generate the required links and names at https://fonts.google.com.
+  // googleFontSans - the sans serif font used in onedrive-vercel-index.
+  googleFontSans: 'Inter',
+  // googleFontMono - the monospace font used in onedrive-vercel-index.
+  googleFontMono: 'Fira Mono',
+  // googleFontLinks -  an array of links for referencing the google font assets.
+  googleFontLinks: ['https://fonts.googleapis.com/css2?family=Fira+Mono&family=Inter:wght@400;500;700&display=swap'],
+
+  // [OPTIONAL] The footer component of your website. You can write HTML here, but you need to escape double
+  // quotes - changing " to \". You can write anything here, and if you like badges, generate some with https://shields.io
+  footer:
+    '<div style="text-align: center;"><audio src="不散的花 - 泠鸢yousa.mp3 controls></audio></div>',
+
+  // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
+  // the directories in which you have .password set. Check the documentation for details.
+  protectedRoutes: ['/🌞 Private folder/u-need-a-password', '/🥟 Some test files/Protected route'],
+
+  // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
+  email: 'lxshlrd@gmail.com',
+
+  // [OPTIONAL] This is an array of names and links for setting your social information and links.
+  // In the latest update, all brand icons inside font awesome is supported and the icon to render is based on the name
+  // you provide. See the documentation for details.
+  links: [
+    {
+      name: 'GitHub',
+      link: '#',
+    },
+  ],
+
+  // This is a day.js-style datetime format string to format datetimes in the app. Ref to
+  // https://day.js.org/docs/en/display/format for detailed specification. The default value is ISO 8601 full datetime
+  // without timezone and replacing T with space.
+  datetimeFormat: 'YYYY-MM-DD HH:mm:ss',
 }
